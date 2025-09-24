53b79b4304fe67c4ba3ccc3691a5805125117b15
dmitrij.gashuk <dmitrij.gashuk@gmail.com>
2025-09-24 22:32:12 +0300

    initial commit

---
+++ src/main/java/com/studentdiary/student_diary/controller/YodaDemoController.java
@@ -0,0 +1,10 @@
+package com.studentdiary.student_diary.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/yoda/hello")
+public class YodaDemoController {
+
+}
---
+++ src/test/java/com/studentdiary/student_diary/StudentDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentdiary.student_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
