657d1febd5aa6718d14a524e58d2ec8bc07aa38b
dmitrij.gashuk <dmitrij.gashuk@gmail.com>
2025-09-27 15:07:14 +0300

    add minor init changes

--- src/main/java/com/studentdiary/student_diary/backend/YodaDemoController.java
+++ src/main/java/com/studentdiary/student_diary/backend/YodaDemoController.java
@@ -1,5 +1,6 @@
-package com.studentdiary.student_diary.controller;
+package com.studentdiary.student_diary.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/yoda/hello")
 public class YodaDemoController {
 
+    @GetMapping("/yoga")
+    public String getTestYoga(){
+        return "Hello, yoga!";
+    }
 }
